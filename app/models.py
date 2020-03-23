d7abd678d6bc1ed09dc3491d07baf46f8fd89ff0
Shamil Murtazaliev <murtazaliev.shamil94@gmail.com>
2020-03-24 00:37:39 +0300

    fix

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class User(db.Model):
     password_hash =  db.Column(db.String(128))
 
     @property
-    def password(selfs):
+    def password(self):
         raise AttributeError('password in not a readable attribute')
 
     @password.setter
